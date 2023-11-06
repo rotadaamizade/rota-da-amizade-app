aa78f1fc57a16a22ad829543428ab7d7fa7ec32b
maiconda <maicondeoliveiradasilva24@gmail.com>
2023-11-06 17:22:01 -0300

    eventos na pagina associado e integração dos municipios com o banco

--- src/components/cityFilter/cityFilter.jsx
+++ src/components/cityFilter/cityFilter.jsx
@@ -1,12 +1,39 @@
 import { useContext, useEffect, useRef, useState } from 'react'
 import './cityFilter.css'
 import { UserContext } from '../../UserContext';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../../config/firebase';
 
 function CityFilter(){
 
     const { globalCity, setGlobalCity } = useContext(UserContext)
     const background = useRef();
     const popup = useRef();
+    const [cities, setCities] = useState([])
+
+    useEffect(() => {
+        getCities()
+    }, [])
+
+    const getCities = async () => {
+        try {
+          const data = await getDocs(collection(db, "municipios"));
+          const citiesData = [];
+      
+          data.forEach((doc) => {
+            const cityData = {
+              municipio: doc.data().municipio,
+              img: doc.data().imgCard.url
+            };
+            
+            citiesData.push(cityData);
+          });
+      
+          setCities(citiesData);
+        } catch (error) {
+          console.error("Erro ao recuperar documentos:", error);
+        }
+    }
   
     const closePopup = () => {
         background.current.style.opacity = '0';
@@ -25,32 +52,6 @@ function CityFilter(){
         popup.current.style.zIndex = '4';
     };
 
-    const cities = [
-        {
-            name: 'Salto Veloso',
-            img: 'https://www.diocesedecacador.org.br/wp-content/uploads/sites/304/2023/03/salnto-1024x576.jpg'
-        },
-        {
-            name: 'Tangará',
-            img: 'https://turismo.tangara.sc.gov.br/uploads/sites/217/2022/12/Tangara-Cidade-Vista-Aerea-scaled-2000x1000.jpg'
-        },
-        {
-            name: 'Joaçaba',
-            img: 'https://bomdiasc.com.br/wp-content/uploads/elementor/thumbs/Jba_29agosto001-1-oqj9p5c395126l9xcdkx6y77ui8cwdh23o0x0y9caw.jpg'
-        },
-        {
-            name: 'Videira',
-            img: 'https://www.passeios.org/wp-content/uploads/2017/11/v-5.jpg'
-        },
-        {
-            name: 'Fraiburgo',
-            img: 'https://www.viagensecaminhos.com/wp-content/uploads/2019/02/hotel-renar-fraiburgo.jpg'
-        },
-        {
-            name: 'Pinheiro Preto',
-            img: 'https://1.bp.blogspot.com/-v9mDwPDP7G8/Xjxte5KY9zI/AAAAAAAAdwM/SLGHdZoOt-AKvOOzSBCCsfxOYf-TSEuvQCLcBGAsYHQ/s1600/aerea..jpg'
-        }
-    ]
 
     return(
     <>
@@ -64,8 +65,8 @@ function CityFilter(){
                 {cities.map((city, index) => (
                 <p key={index} onClick={() => {
                     closePopup()
-                    setGlobalCity(city.name)
-                }}>{city.name}</p>
+                    setGlobalCity(city.municipio)
+                }}>{city.municipio}</p>
                 ))}
             </div>
         </div>
@@ -74,7 +75,7 @@ function CityFilter(){
                     <img src="https://static.todamateria.com.br/upload/pa/is/paisagem-natural-og.jpg" alt="" className={`city-img ${globalCity === '' ? 'selected' : ''}`}/>
             {
                 cities.map((city, index) => (
-                    <img key={index} className={`city-img ${globalCity === city.name ? 'selected' : 'not-selected'}`} src={city.img} alt="" />
+                    <img key={index} className={`city-img ${globalCity === city.municipio ? 'selected' : 'not-selected'}`} src={city.img} alt="" />
                 ))
             }  
             <div className='gradient'></div>
